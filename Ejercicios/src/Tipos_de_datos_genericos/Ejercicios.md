19d4b6ea7317d003fc1ce118c8a8672bc045d7ee
Victor Gallegos <316160456@ciencias.unam.mx>
2021-07-30 18:05:06 -0500

    Tipos Abstractos de datos

---
+++ Ejercicios/src/Tipos_de_datos_genericos/Ejercicios.md
@@ -0,0 +1,6 @@
+# EJERCICIO
+
+Implementa una clase Lista.java con los métodos add,
+delete, get y size pero que cuando hagamos get
+nos borre el elemento de la lista, y queremos
+que la lista solo sea para Integers no necista ser funcional.
---
+++ Ejercicios/src/Tipos_de_datos_genericos/Lista.java
@@ -0,0 +1,24 @@
+package Ejercicios.src.Tipos_de_datos_genericos;
+
+public class Lista {
+    private ArrayList<Integer> lista = new ArrayList<Integer>();
+
+    public Lista() {
+    }
+
+    public Integer get(int index) {
+        return lista.remove(index);
+    }
+
+    public void delete(int index) {
+        lista.remove(index);
+    }
+
+    public void add(Integer integer) {
+        lista.add(integer);
+    }
+
+    public int size() {
+        return lista.size();
+    }
+}
